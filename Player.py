260d248dcb0b7b6e23096c86aa3f8e9fc54aae9e
willsonr8 <willsonr8@gmail.com>
2023-12-08 17:08:19 -0500

    Fantasy data on graph

--- Player.py
+++ Player.py
@@ -1,20 +1,19 @@
 import json
 class Player_Info:
-    def __init__(self, name, school, headshot):
+    def __init__(self, name, ID, headshot):
         self.name = name
-        self.school = school
+        self.ID = str(ID)
         self.headshot = headshot
+        self.fantasy_points = []
 
     @classmethod
     def from_api_response(cls, api_response):
 
         parsed_data = json.loads(api_response)
-        if "error" in parsed_data:
-            return cls(None, None)
 
         player_info = parsed_data["body"][0]
         name = player_info.get("espnName")
-        school = player_info.get("school")
+        ID = player_info.get("playerID")
         headshot = player_info.get("espnHeadshot")
 
-        return cls(name, school, headshot)
+        return cls(name, ID, headshot)
--- main.py
+++ main.py
@@ -5,6 +5,23 @@ from Player import Player_Info
 import pandas as pd
 import numpy as np
 
+def validPlayer(name):
+    conn = http.client.HTTPSConnection("tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com")
+
+    headers = {
+        'X-RapidAPI-Key': "00d7863507msh695c4dd7170a696p12d9efjsnfe4d06584f4b",
+        'X-RapidAPI-Host': "tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com"
+    }
+
+    conn.request("GET", f"/getNFLPlayerInfo?playerName={name}&getStats=true", headers=headers)
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+    if "error" in data:
+        return False
+    else:
+        return True
+
+
 def inputHandler(name):
     normInput = ""
     for c in name:
@@ -28,13 +45,38 @@ def pullPlayer(name):
 
     res = conn.getresponse()
     data = res.read().decode("utf-8")  # Decode the response to a string
+
     player = Player_Info.from_api_response(data)
-    if player.name is None:
-        st.write("Player not found")
-    else:
-        st.write(player.name)
-        st.write(player.school)
-        st.image(player.headshot)
+    return player
+
+
+def pullFantasyInfo(ID):
+    conn = http.client.HTTPSConnection("tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com")
+
+    headers = {
+        'X-RapidAPI-Key': "00d7863507msh695c4dd7170a696p12d9efjsnfe4d06584f4b",
+        'X-RapidAPI-Host': "tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com"
+    }
+
+    conn.request("GET",
+                 f"/getNFLGamesForPlayer?playerID={ID}&fantasyPoints=true&twoPointConversions=2&passYards=.04&passTD=4&passInterceptions=-2&pointsPerReception=1&carries=.2&rushYards=.1&rushTD=6&fumbles=-2&receivingYards=.1&receivingTD=6&targets=0&defTD=6",
+                 headers=headers)
+
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+    fantasy_points = []
+
+    parsed_data = json.loads(data)
+
+    print(parsed_data)
+
+    for key in parsed_data["body"].keys():
+        fantasy_points.append(parsed_data["body"][key].get("fantasyPointsDefault").get("PPR"))
+
+    return fantasy_points
+
+
+
 
 if __name__ == '__main__':
 
@@ -44,4 +86,21 @@ if __name__ == '__main__':
 
     name = inputHandler(st.session_state.name)
 
-    pullPlayer(name)
+    if validPlayer(name) is False:
+        st.write("Player not found")
+
+    else:
+
+        player = pullPlayer(name)
+
+        player.fantasy_points = pullFantasyInfo(player.ID)
+
+        st.write(player.name)
+
+        st.write(player.ID)
+
+        st.image(player.headshot)
+
+        print(player.fantasy_points[0])
+
+        st.line_chart(player.fantasy_points)
